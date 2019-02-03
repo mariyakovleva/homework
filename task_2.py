7f0f8202f66486594c0f29baee9c61593234244a
Your Name <you@example.com>
2019-02-03 19:52:42 +0700

    first version

--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
-#1
+#2.1
 from random import randrange as rnd
 A=[rnd(10) for i in range(10)]
 print(A)
@@ -7,13 +7,13 @@ k1=A[k:]
 k2=A[:k]
 k3=k1+k2
 print(k3)
-#2
+#2.2
 A[1],A[2]=A[2],A[1]
 A[3],A[4]=A[4],A[3]
 A[5],A[6]=A[6],A[5]
 A[7],A[8]=A[8],A[7]
 print(A)
-#3
+#2.3
 A[0],A[9]=A[9],A[0]
 A[1],A[8]=A[8],A[1]
 A[2],A[7]=A[7],A[2]
